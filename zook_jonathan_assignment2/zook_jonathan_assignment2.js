cdd0ed98af5eeb268818f7ca40391dd93408101d
Jonathan Zook <ijaz@Jonathans-MacBook-Pro.local>
2014-10-09 19:34:19 -0400

    first procedure in

--- zook_jonathan_assignment2/zook_jonathan_assignment2.js
+++ zook_jonathan_assignment2/zook_jonathan_assignment2.js
@@ -1,1 +1,33 @@
-alert("JavaScript works!");
+// Jonathan Zook
+// 10-09-2014
+// Project 2
+// Theme
+
+//Global Variables
+
+var romantic = "yes"
+var realName = "So-and-so"
+
+//Procedures
+
+var romance = function (romantic, realName) {
+    if (romantic === "yes") {
+        console.log(realName + ", romance is out there for the right person.");
+    }else {
+        console.log(realName + ", courtly romance is not always about love.")
+    };  //"romantic" is re-used here but is not the global variable   
+};  //romance procedure
+
+
+//Functions
+
+
+
+
+//Project 2 Start
+
+realName = prompt ("What is your name?");
+
+romantic = prompt ("Are you a romantic, yes or no?");
+
+romance(romantic, realName);
