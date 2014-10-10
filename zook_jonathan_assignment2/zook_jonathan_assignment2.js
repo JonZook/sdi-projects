9c2ac2e338ce7aa811d01724d5626ea83d86a6d3
Jonathan Zook <ijaz@Jonathans-MacBook-Pro.local>
2014-10-09 20:31:58 -0400

    string function in

--- zook_jonathan_assignment2/zook_jonathan_assignment2.js
+++ zook_jonathan_assignment2/zook_jonathan_assignment2.js
@@ -5,8 +5,11 @@
 
 //Global Variables
 
-var romantic = "yes"
-var realName = "So-and-so"
+var romantic = "yes";
+var realName = "So-and-so";
+var knightMe = true;
+var royalTitle = "Sir";
+var fullName = "Sir So-and-so"
 
 //Procedures
 
@@ -14,13 +17,33 @@ var romance = function (romantic, realName) {
     if (romantic === "yes") {
         console.log(realName + ", romance is out there for the right person.");
     }else {
-        console.log(realName + ", courtly romance is not always about love.")
+        console.log(realName + ", courtly romance is not always about love.");
     };  //"romantic" is re-used here but is not the global variable   
 };  //romance procedure
 
 
 //Functions
 
+var getTitle = function (knightMe) {
+    var title;
+    if (knightMe === true) {
+        title = "Sir";
+    }else {
+        title = "Princess";    
+    }
+    return title
+};  //getTitle function (not part of assignment)
+
+var getName = function (royalTitle, realName) {
+    var fullName = royalTitle + " " + realName;
+    return fullName;
+};  //getName function
+
+
+var joust = function (knightMe, realName) {
+    
+    
+}; //joust function
 
 
 
@@ -31,3 +54,13 @@ realName = prompt ("What is your name?");
 romantic = prompt ("Are you a romantic, yes or no?");
 
 romance(romantic, realName);
+
+knightMe = confirm("Courtly romance in medieval times was between a Knight and a Princess.  Are you a Knight?");
+
+royalTitle = getTitle(knightMe);
+
+fullName = getName(royalTitle, realName);
+
+console.log(fullName);
+
+joust(knightMe, realName);
