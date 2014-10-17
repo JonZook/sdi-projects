4ba59025f6ed24e93366224c315a32f5fa9ddd25
Jonathan Zook <ijaz@Jonathans-MacBook-Pro.local>
2014-10-16 20:36:09 -0400

    json data added

---
+++ zook_jonathan_assignment3/json.js
@@ -0,0 +1,51 @@
+// Jonathan Zook
+// 10-16-2014
+// Project 3
+// Courtly Romance
+// JSON Data File
+
+var json = {
+    "knights": [
+        {
+            "knightName": "Sir Ralph",
+            "winner": "win"
+        },
+        {
+            "knightName": "Sir Steven",
+            "winner": "win"
+        },
+        {
+            "knightName": "Sir Ted",
+            "winner": "win"
+        },
+        {
+            "knightName": "Sir Chuck",
+            "winner": "win"
+        },
+        {
+            "knightName": "Sir Larry",
+            "winner": "win"
+        },
+        {
+            "knightName": "Sir Brad",
+            "winner": "win"
+        },
+        {
+            "knightName": "Sir Phil",
+            "winner": "win"
+        },
+        {
+            "knightName": "Sir Craig",
+            "winner": "win"
+        },
+        {
+            "knightName": "Sir Timmy",
+            "winner": "win"
+        },
+        {
+            "knightName": "Sir Barry",
+            "winner": "win"
+        },
+    ]
+}
+
