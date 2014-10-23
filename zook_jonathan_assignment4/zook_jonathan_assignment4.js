8cdeef89a6212769da5e749c7c32db44f55f0432
Jonathan Zook <ijaz@Jonathans-MacBook-Pro.local>
2014-10-23 17:49:34 -0400

    urlVerify Complete

--- zook_jonathan_assignment4/zook_jonathan_assignment4.js
+++ zook_jonathan_assignment4/zook_jonathan_assignment4.js
@@ -3,14 +3,18 @@
 // Project 4
 // Collection of Functions
 
-var phoneNumberBad = "123-45-67"
-var phoneNumberGood = "407-695-0100"
-var phoneNumberShort = "407-695-010"
-var emailGood1 = "myemail@server.net"
-var emailGood2 = "a@server.co"
-var emailBad1 = "myemail@server"
-var emailBad2 = "myemail@.com"
-var emailBad3 = "server.com"
+var phoneNumberBad = "123-45-67";
+var phoneNumberGood = "407-695-0100";
+var phoneNumberShort = "407-695-010";
+var emailGood1 = "myemail@server.net";
+var emailGood2 = "a@server.co";
+var emailBad1 = "myemail@server";
+var emailBad2 = "myemail@.com";
+var emailBad3 = "server.com";
+var webGood1 = "http://web.com";
+var webGood2 = "https://web.com";
+var webBad1 = "http:/web.com";
+var webBad2 = "web.com";
 
 
 //Functions
@@ -29,7 +33,19 @@ var emailVerify = function (email) {
     } else {
         return false;
     };
-};
+};  // emailVerify Function
+
+var urlVerify = function (url) {
+    if (url.substring(0,8) === "https://" || url.substring(0,7) === "http://") {
+        return true;
+    } else {
+    return false;
+    };
+};  // urlVerify Function
+
+
+
+
 
 //Program
 
@@ -44,3 +60,9 @@ console.log("Is " + emailGood2 + " in the pattern of an email address? Answer: "
 console.log("Is " + emailBad1 + " in the pattern of an email address? Answer: " + emailVerify(emailBad1));
 console.log("Is " + emailBad2 + " in the pattern of an email address? Answer: " + emailVerify(emailBad2));
 console.log("Is " + emailBad3 + " in the pattern of an email address? Answer: " + emailVerify(emailBad3));
+
+console.log("Let's check some URLs!");
+console.log("Does " + webGood1 + " start with http:// or https://? Answer: " + urlVerify(webGood1));
+console.log("Does " + webGood2 + " start with http:// or https://? Answer: " + urlVerify(webGood2));
+console.log("Does " + webBad1 + " start with http:// or https://? Answer: " + urlVerify(webBad1));
+console.log("Does " + webBad2 + " start with http:// or https://? Answer: " + urlVerify(webBad2));
